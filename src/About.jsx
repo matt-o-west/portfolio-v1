be2f68b1f1f00e98cc106af76b417cdc1ce21c2d
matt-o-west <matt.omalley.west@gmail.com>
2022-12-31 21:20:11 -0500

    styling about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className='grid grid-cols-2 w-full mt-0 col-auto px-4 pt-20 space-x-2'>
+      <div>
+        <h1 className='font-600 text-4xl'>
+          hey, i'm <span className='decoration-teal-400'>matt</span>
+        </h1>
+        <p>based in the us, i'm a frontend developer building web apps</p>
+      </div>
+      <div className='w-1/3 px-2 absolute right-8 top-0 z-0 object-cover md:object-scale-down '>
+        <img
+          src='/images/image-profile-desktop.webp'
+          alt='profile'
+          className='w-full z-auto'
+        />
+      </div>
+    </div>
+  )
 }
 
 export default About
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,16 +3,9 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div className='grid grid-cols-2 w-100 pt-3 px-3'>
+    <div className='grid grid-cols-2 w-100 h-0 pt-3 px-10 z-50'>
       <span className='font-600 w-1/3'>matt-o-west</span>
-      <div className='flex justify-end space-x-4'>
-        <a href='' className='font-600'>
-          <img
-            src='/images/icon-linkedin.svg'
-            alt='github'
-            className='hover:scale-125 hover:transition-all hover:ease-in-out'
-          />
-        </a>
+      <div className='flex space-x-4 pl-60 z-20 lg:flex-auto'>
         <a href='' className='font-600'>
           <img
             src='/images/icon-github.svg'
@@ -39,7 +32,7 @@ const Nav = () => {
           <img
             src='/images/icon-codewars.svg'
             alt='codewars'
-            className='hover:scale-125 hover:transition-all hover:ease-in-out'
+            className=' hover:scale-125 hover:transition-all hover:ease-in-out w-6 fill-white'
           />
         </a>
       </div>
