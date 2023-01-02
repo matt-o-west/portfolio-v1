3c1e2bc9abc503312ad8b0aa5f65bbb80f265973
matt-o-west <matt.omalley.west@gmail.com>
2023-01-02 18:56:43 -0500

    complete form styling

--- src/helpers/ContactForm.jsx
+++ src/helpers/ContactForm.jsx
@@ -14,12 +14,12 @@ const ContactForm = () => {
   }
 
   const handleSubmit = (event) => {
-    alert('A name was submitted: ' + state.value)
+    alert('A name was submitted: ' + name.value)
     event.preventDefault()
   }
 
   return (
-    <div>
+    <div className='row-span-2 pl-12'>
       <form
         onSubmit={handleSubmit}
         className='grid grid-rows-3 grid-cols-1 pt-6'
@@ -29,7 +29,7 @@ const ContactForm = () => {
             type='text'
             value={name.value}
             onChange={handleNameChange}
-            className='ml-4 p-2 bg-zinc-800 placeholder-zinc-600 focus:outline-none focus:border-teal-400 focus:ring-2'
+            className='ml-4 p-2 bg-zinc-800 placeholder-zinc-600 focus:outline-none focus:border-teal-400 focus:ring-2 focus:ring-teal-400 border-b-4 border-teal-400 hover:border-teal-200 required'
             placeholder='NAME'
           />
         </label>
@@ -38,21 +38,21 @@ const ContactForm = () => {
             type='email'
             value={email.value}
             onChange={handleEmailChange}
-            className='ml-4 p-2 bg-zinc-800 placeholder-zinc-600 invalid:border-red-600 invalid:border-4 focus:outline-none focus:border-teal-400 focus:ring-2'
+            className='ml-4 p-2 bg-zinc-800 placeholder-zinc-600 invalid:border-red-600 invalid:border-4 focus:outline-none focus:border-teal-400 focus:ring-2 focus:ring-teal-400 border-b-4 border-teal-400 hover:border-teal-200 required'
             placeholder='EMAIL'
           />
         </label>
 
         <input
           type=''
-          className='h-20 w-4/5 m-2 bg-zinc-800 placeholder-zinc-600 focus:outline-none focus:border-teal-400 focus:ring-2'
-          placeholder='MESSAGE'
+          className='h-20 w-4/5 m-2 bg-zinc-800 placeholder-zinc-600 focus:outline-none focus:border-teal-400 focus:ring-2 focus:ring-teal-400 border-b-4 border-teal-400 hover:border-teal-200 required'
+          placeholder=' MESSAGE'
         />
 
         <button
           type='submit'
           value='Submit'
-          className='w-1/5 shadow bg-teal-500 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
+          className='w-1/5 min-w-min shadow bg-teal-600 hover:bg-teal-400 hover:text-zinc-900 hover:font-extrabold focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded'
         >
           SUBMIT
         </button>
