6a471b9061b43d742210136c9f8a22a7b45b293c
matt-o-west <matt.omalley.west@gmail.com>
2023-01-03 20:12:21 -0500

    tweaking styling

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Projects = () => {
   return (
     <div>
-      <h1 className='font-600 text-5xl pt-20 pl-20'>Projects</h1>
+      <h1 className='font-600 text-5xl pt-20 pl-20 w-1'>Projects</h1>
+      <button className='underline decoration-teal-400 decoration-2 my-8 hover:scale-110 hover:transition-all hover:ease-in-out hover:text-teal-400 hover:bg-blend-darken hover:bg-zinc-800 px-2 rounded-sm'>
+        CONTACT ME
+      </button>
       <div className='grid grid-cols-3 gap-x-10 gap-y-10 pt-10 pl-20 pb-32'>
         <div className='w-full px-6 object-cover md:object-scale-down'>
           <img
