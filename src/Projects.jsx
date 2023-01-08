a6cd67b1f0d4d73cea4a3ee8d15aeab1b1dddafd
matt-o-west <matt.omalley.west@gmail.com>
2023-01-07 22:49:08 -0500

    working on project overlay/hover states

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -19,21 +19,43 @@ const Projects = () => {
           <p className='font-600 text-2xl'>DESIGN PORTFOLIO</p>
           <p className='font-400 text-xl'>Typescript Vite Emotion</p>
         </div>
-        <div className='w-full z-auto my-2 hover:opacity-75 hover:bg-zinc-400 hover:bg-cover hover: cursor-pointer'>
-          <img
-            src='/images/thumbnail-project-2-large.webp'
-            alt='project-one'
-            className='w-full z-auto py-2'
-          />
-          <p className='font-600 text-2xl'>E-LEARNING APP</p>
-          <p className='font-400 text-xl'>Typescript Astro Tailwind</p>
+        <div class='group'>
+          <div className='hidden group-hover:block absolute max-w-full opacity-20 bg-zinc-500 object-cover justify-center align-center'>
+            <button className='underline py-2 col-span-1 w-1/8 decoration-teal-400 decoration-2 hover:scale-110 hover:transition-all hover:ease-in-out hover:text-teal-400 hover:bg-blend-darken hover:bg-zinc-800 px-2 rounded-sm'>
+              LINK
+            </button>
+            <button>
+              <img className='w-6' src='/images/icon-github.svg' alt='github' />
+            </button>
+          </div>
+          <div className='w-full px-6 object-cover md:object-scale-down'>
+            <img
+              src='/images/thumbnail-project-2-large.webp'
+              alt='project-one'
+              className='w-full z-auto py-2'
+            />
+            <p className='font-600 text-2xl'>E-LEARNING APP</p>
+            <p className='font-400 text-xl'>Typescript Astro Tailwind</p>
+          </div>
         </div>
-        <div className='w-full z-auto my-2 hover:opacity-75 hover:bg-zinc-400 hover:bg-cover hover: cursor-pointer'>
+        <div className='w-full px-6 object-cover md:object-scale-down'>
           <img
             src='/images/thumbnail-project-3-large.webp'
             alt='project-one'
             className='w-full z-auto my-2 hover:opacity-75 hover:bg-zinc-400 hover:bg-cover hover: cursor-pointer'
           />
+          <div className='hidden group-hover:block absolute opacity-20 bg-zinc-500 object-cover '>
+            <button className='underline justify-self-end py-2 col-span-1 mt-24 mr-10 w-1/8 decoration-teal-400 decoration-2 hover:scale-110 hover:transition-all hover:ease-in-out hover:text-teal-400 hover:bg-blend-darken hover:bg-zinc-800 px-2 rounded-sm'>
+              LINK
+            </button>
+            <button>
+              <img
+                src='/images/icon-github.svg'
+                alt='github'
+                className='w-6 opacity-100'
+              />
+            </button>
+          </div>
           <a
             href='/images/thumbnail-project-3-large.webp'
             target='_blank'
