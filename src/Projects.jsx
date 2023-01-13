7eef2057e3e36a2589ea00434c62322151e72c15
matt-o-west <matt.omalley.west@gmail.com>
2023-01-12 20:14:43 -0500

    add mobile buttons

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = ({ handleClick }) => {
   return (
     <div>
       <div className='grid grid-cols-2 w-full'>
-        <h1 className='col-span-1 font-600 text-5xl pt-20 pl-20 justify-items-center'>
+        <h1 className='col-span-1 font-600 text-5xl pt-20 pl-20 justify-items-center mb-4'>
           projects
         </h1>
         <button
@@ -15,9 +15,9 @@ const Projects = ({ handleClick }) => {
           CONTACT ME
         </button>
       </div>
-      <div className='grid grid-cols-3 gap-x-10 gap-y-10 pt-1 px-12 pb-32  lg:grid-cols-3 sm:grid-cols-1 min-[320px]:grid-cols-1 max-[360px]:p-8'>
+      <div className='grid grid-cols-3 gap-x-10 gap-y-2 sm:gap-y-2 xs:gap-y-2 pt-1 px-12 pb-32 lg:grid-cols-3 sm:grid-cols-1 min-[320px]:grid-cols-1 max-[360px]:p-8'>
         {/* Card 1 */}
-        <div className='py-4'>
+        <div className='mt-6'>
           <div className='shadow-lg group container rounded-lg bg-zinc-900  max-w-sm flex justify-center items-center mx-auto content-div project-one ease-in duration-75'>
             <div>
               <div className='w-full image-cover rounded-t-md'>
@@ -28,6 +28,7 @@ const Projects = ({ handleClick }) => {
                       alt='github'
                       className='w-6 h-6'
                     />
+
                     <img
                       src='https://img.icons8.com/color/48/000000/react-native.png'
                       alt='github'
@@ -65,8 +66,17 @@ const Projects = ({ handleClick }) => {
             </div>
           </div>
         </div>
+        {/* Mobile buttons */}
+        <div className='grid grid-cols-2 lg:hidden md:hidden p-0 justify-self-center space-x-4'>
+          <button className='underline text-xl py-2 px-4 mt-6 my-2 w-1/8 decoration-teal-400 text-teal-400 decoration-2 hover:scale-110 hover:transition-all hover:ease-in-out hover:text-teal-400 hover:decoration-teal-400 hover:bg-blend-darken hover:bg-zinc-800 rounded-md'>
+            DEMO
+          </button>
+          <button className='py-0 px-6 lg:mt-6 w-1/8 hover:scale-110 hover:transition-all hover:ease-in-out hover:bg-blend-lighten rounded-md'>
+            <img src='/images/github-mobile.svg' />
+          </button>
+        </div>
         {/* Card 2 */}
-        <div className='py-4 transition-all '>
+        <div className='mt-6 transition-all '>
           <div className='shadow-lg group container rounded-lg bg-zinc-900  max-w-sm flex justify-center items-center mx-auto content-div project-two  ease-in duration-75'>
             <div>
               <div className='w-full image-cover rounded-t-md'>
@@ -115,7 +125,7 @@ const Projects = ({ handleClick }) => {
           </div>
         </div>
         {/* Card 3 */}
-        <div className='py-4 transition-all '>
+        <div className='mt-6 transition-all '>
           <div className='shadow-lg group container rounded-lg bg-zinc-900  max-w-sm flex justify-center items-center mx-auto content-div project-three  ease-in duration-75'>
             <div>
               <div className='w-full image-cover rounded-t-md'>
